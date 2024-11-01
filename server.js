52ea4601dca184fa0cd9d2c05dbd4239c736b340
Manas <manas.shetty04@gmail.com>
2024-11-01 23:24:53 +0530

    added authentication functionality to server.js

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ require('dotenv').config();
 
 const sequelize = require('./config/database');
 
+sequelize.authenticate()
+  .then(() => {
+    console.log('Database connected successfully.');
+  })
+  .catch((error) => {
+    console.error('Unable to connect to the database:', error);
+  });
+
 sequelize.sync({ force: false })  // Set to 'true' only during development to recreate tables
   .then(() => console.log("Database & tables created!"))
   .catch((error) => console.log("Error creating database", error));
